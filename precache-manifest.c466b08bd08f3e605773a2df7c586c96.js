a3d5da0f0b5a966e4365da4d78b70ec9ffbb7117
dev-dk <krishnadev.lead@gmail.com>
2019-02-25 00:16:33 +0530

    Updates

---
+++ precache-manifest.c466b08bd08f3e605773a2df7c586c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "b6a657fee36c89e3b465",
+    "url": "/portfolio/static/js/main.b6a657fe.chunk.js"
+  },
+  {
+    "revision": "459567d6f6dcb83f4c71",
+    "url": "/portfolio/static/js/2.459567d6.chunk.js"
+  },
+  {
+    "revision": "b6a657fee36c89e3b465",
+    "url": "/portfolio/static/css/main.deb42c7f.chunk.css"
+  },
+  {
+    "revision": "459567d6f6dcb83f4c71",
+    "url": "/portfolio/static/css/2.a94cc24a.chunk.css"
+  },
+  {
+    "revision": "3dcb92511b921e160bc86a1fd1997704",
+    "url": "/portfolio/index.html"
+  }
+];
